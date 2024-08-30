e2dcf06c1483cc97ae00b5ce26727f5172b2cb24
CamillaCRibeiro <126510468+CamillaCRibeiro@users.noreply.github.com>
2024-08-30 01:41:16 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio Pessoal</title>
+    <link rel="stylesheet" href="css/estiloindex.css">
+</head>
+<body>
+    <?php include "paginas/navbar.html"?>
+    <section class="home">    
+        <div class="img_eu">
+            <img class="eu" src="img/euu.jpg">
+        </div> 
+        <div class="content">
+        <h1>Camilla<br>Corrêa Ribeiro</h1>
+        <p>Olá! Tenho 19 anos e faço faculdade de Análise e Desenvolvimento de Sistemas no IFSP campus Araraquara.
+        Criei este portfolio para mostrar um pouco do que eu fiz durante o primeiro semestre na disciplina de
+        Construção de Páginas WEB e usá-lo como projeto prático para apresentar o que aprendi. 
+        </p>
+        </div>
+    </section>
+</body>
+</html>

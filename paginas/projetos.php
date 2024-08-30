a5df84281453e540c47883622f77539561e38d21
CamillaCRibeiro <126510468+CamillaCRibeiro@users.noreply.github.com>
2024-08-30 01:44:13 -0300

    Add files via upload

---
+++ paginas/projetos.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projetos</title>
+    <link rel="stylesheet" href="../css/estiloprojetos.css"
+</head>
+<body>
+    <?php include "navbar.html"?>
+
+    <h1>Projetos</h1>
+
+    <div class="container">
+        <div class="sitelista">
+            <div class="imagem">
+                <img class="print" src="../img/printlista.png">
+            </div>
+            <div class="bolinha">
+                <h3>Minha<br> Biblioteca</h3>
+            </div>
+            <div class="texto">
+                <p>O site foi disponibilizado pela professora Roberta Akemi no ambiente virtual Moodle e a proposta era alterar e adicionar páginas neste site.</p>
+            </div>
+        </div>
+        <div class="atividadeextra">
+            <div class="imagem">
+                <img class="print" src="../img/printatividade.png">
+            </div>
+            <div class="bolinha">
+                <h3>Atividade <br> Extra</h3>
+            </div>
+            <div class="texto">
+                <p>Foi desenvolvido um site com duas páginas HTML contendo tabela, formulário, link externo, link para outra página do mesmo site, link para a mesma página, uma imagem, um vídeo e uma lista.</p>
+            </div>
+        </div>
+        <div class="projetopratico">
+            <div class="imagem">
+                <img class="print" src="../img/printprojeto.png">
+            </div>
+            <div class="bolinha">
+                <h3>Projeto <br> Prático</h3>
+            </div>
+            <div class="texto">
+                <p>Desenvolvimento de um website utilizando HTML e CSS.</p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>

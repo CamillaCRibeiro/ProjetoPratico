d5776dd263d70bdcc7f685029be08c3ed5174def
CamillaCRibeiro <126510468+CamillaCRibeiro@users.noreply.github.com>
2024-08-30 01:44:43 -0300

    Add files via upload

---
+++ paginas/contato.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contato</title>
+    <link rel="stylesheet" href="../css/estilocontato.css">
+    <script src="https://kit.fontawesome.com/df721bc31d.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <?php include "navbar.html"?>
+
+    <div class="retangulo">
+    <h1>Quer entrar em contato comigo?</h1>
+    </div>
+
+    <div class="content">
+        <a href="mailto:camilla.correaribeiro04@gmail.com">camilla.correaribeiro04@gmail.com</a><br><br>
+        <a href="tel:+5516994437299">(16)99443-7299</a>
+    </div>
+
+    <div class="redes">
+        <a class="icon" href="https://www.linkedin.com/in/camilla-corrêa-ribeiro-678a91252/" target="_blank"><i class="fa-brands fa-linkedin" style="color: #e8e3de;"></i></a>
+        <a class="icon" href="https://www.instagram.com/camilluska/" target="_blank"><i class="fa-brands fa-square-instagram" style="color: #e8e3de;"></i></a>
+        <a class="icon" href="https://www.facebook.com/camilluska" target="_blank"><i class="fa-brands fa-square-facebook" style="color: #e8e3de;"></i></a>
+    </div>
+</body>
+</html>
